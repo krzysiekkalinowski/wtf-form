cd95d053967d7342f50445cf7afadc1f8354a7e3
Krzyszot Kalinowski <krzykalinowski@gmail.com>
2023-12-29 22:49:36 +0100

    removed cached elements

--- dist/index.de158e3a.js
+++
@@ -1,8 +0,0 @@
-const holder = document.querySelector(".form-quantity__quantity-holder--js");
-holder.textContent = quantity.value;
-quantity.addEventListener("input", ()=>{
-    const holder = document.querySelector(".form-quantity__quantity-holder--js");
-    holder.textContent = quantity.value;
-});
-
-//# sourceMappingURL=index.de158e3a.js.map
